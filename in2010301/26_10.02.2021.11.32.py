9338edeb66ce2ae72ba44d29919f8848e4e370b1
svetochpcnine <a.timin2003@mail.ru>
2021-02-12 14:52:56 +0300

    Done 26.py in in2010301

--- in2010301/26_10.02.2021.11.32.py
+++ in2010301/26_10.02.2021.11.32.py
@@ -7,6 +7,23 @@ def addLineToCurrentModel(line):
     dadaDict[model].append([price, quantity])
 
 
+def getIdListWithLowestPrice(inputList):
+    localId = 0
+    for idx in range(0, len(inputList)):
+        if inputList[idx][0] < inputList[localId][0]:
+            localId = idx
+    return localId
+
+
+def getSortedListByPriceLowHigh(inputList):
+    outputList = []
+    while len(inputList) > 0:
+        newListsId = getIdListWithLowestPrice(inputList)
+        outputList.append(inputList[newListsId])
+        inputList.pop(newListsId)
+    return outputList
+
+
 dadaDict = {
     'A': [],
     'B': []
@@ -15,4 +32,25 @@ with open('C:/Users/Учитель/Desktop/Новая папка/Informatics/in2
     budget = int(data.readline().split(' ')[-1])
     for line in data:
         localArr = addLineToCurrentModel(line)
-print(dadaDict)
+# print(dadaDict)
+dadaDict = {
+    'A': getSortedListByPriceLowHigh(dadaDict['A']),
+    'B': getSortedListByPriceLowHigh(dadaDict['B'])
+}
+# print(dadaDict['A'])
+# print('HEY')
+# print(dadaDict['B'])
+globalId = 0
+quantity = 0
+while budget > 0 and globalId < len(dadaDict['A']):
+    while budget > 0 and dadaDict['A'][globalId][-1] > 0:
+        if budget - dadaDict['A'][globalId][0] >= 0:
+            quantity += 1
+            lastNum = dadaDict['A'][globalId][0]
+        budget -= lastNum
+    globalId += 1
+else:
+    if budget < 0:
+        budget += lastNum
+print(budget, quantity)
+# 4 76923
