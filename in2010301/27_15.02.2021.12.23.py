24963b91820265e262160059de5cb4cbc602ea3c
katenok <a.timin2003@mail.ru>
2021-02-15 20:47:00 +0300

    Added file-reader and list-transformer-function

--- in2010301/27_15.02.2021.12.23.py
+++ in2010301/27_15.02.2021.12.23.py
@@ -1,2 +1,23 @@
-link = 'C:/Users/Учитель/Desktop/Новая папка/Informatics/in2010301/27/'
-fileName = '27-{}.txt'.format(input('Введите A или B'))
+# link = 'C:/Users/Учитель/Desktop/Новая папка/Informatics/in2010301/27/'
+link = 'E:/Py_scr/stat_grad/Informatics/in2010301/27/'
+fileName = input('Введите A или B: ')
+
+
+def getNumsListFromStrsList(strsList):
+    result = []
+    for el in strsList:
+        result.append(int(el))
+    return result
+
+
+with open(f'{link}27-{fileName}.txt', 'r') as data:
+    print(data.readline())
+    for line in data:
+        if fileName == 'A':
+            # print(line.split('  '))
+            localList = getNumsListFromStrsList(line.split('  '))
+            # print(localList)
+        elif fileName == 'B':
+            # print(line.split(' '))
+            localList = getNumsListFromStrsList(line.split(' '))
+            # print(localList)
