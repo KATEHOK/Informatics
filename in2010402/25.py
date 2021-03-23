40acd59e0973f4c4f03d0a421e43f9ef2d82c6da
svetochpcnine <a.timin2003@mail.ru>
2021-03-23 18:44:03 +0300

    sfwef

--- in2010402/25.py
+++ in2010402/25.py
@@ -1,15 +1,18 @@
 def isRight(num):
     counter = 0
-    for x in range(1, num // 2 + 1, 2):
+    for x in range(3, num // 3 + 1, 2):
         if num % x == 0:
             counter += 1
-            if counter > 4:
+            print(num, x, counter)
+            if (num % 2 != 0 and counter > 3) or counter > 4:
                 return False
-    if counter == 4:
+
+    if (num % 2 != 0 and counter == 3) or counter == 4:
         return True
     return False
 
 
 for num in range(45000000, 50000001):
     if isRight(num):
-        print(num)
+        print(num, 'RIGHT')
+# -------^-WRONG-^--------
